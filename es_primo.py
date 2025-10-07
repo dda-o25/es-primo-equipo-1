8bfafdfc3d606288d8ff455521ffc8ed1deed646
Akemi-22 <akemi.olvera@iteso.mx>
2025-10-07 13:35:02 -0600

    primer intento

--- es_primo.py
+++ es_primo.py
@@ -1,15 +1,29 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Alumnos:
+
+Fecha:07/10/25
+Determinar si un número dado es primo
 """
 
 # Declaraciones
-CONSTANTE = valor
+
 
 # Entradas
-entrada = input()
+numero = int(input("Introduzca un número: "))
 
 # Proceso
+x = 2
+p = 0
+primo = f"El número {numero} no es primo"
+while x >= 2 and x <= numero:
+    if numero % x == 0:
+        p += 1
+    x += 1
+if p > 1:
+    primo
+elif p == 1:
+    primo = f"El número {numero} sí es primo"
 
 
 # Salidas
-print(salida)
+print(primo)
