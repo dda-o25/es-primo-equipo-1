212befe0cec7893fc832464036e3c61263cae7ec
Akemi-22 <akemi.olvera@iteso.mx>
2025-10-07 19:45:55 -0600

    segundo

--- es_primo.py
+++ es_primo.py
@@ -1,6 +1,9 @@
 """
 Alumnos:
-
+Leonardo Jesús Álvarez Fernández
+Bruce de Bary Hernández Robles
+José María Meléndrez Varela
+Akemi Clarissa Olvera Arao
 Fecha:07/10/25
 Determinar si un número dado es primo
 """
@@ -12,16 +15,16 @@ Determinar si un número dado es primo
 numero = int(input("Introduzca un número: "))
 
 # Proceso
+# 'x' inicia siendo igual a dos porque todos los números son divisibles entre 1
+# 'd' indica la cantidad de números por los que la variable 'numero' es divisble
 x = 2
-p = 0
+d = 0
 primo = f"El número {numero} no es primo"
 while x >= 2 and x <= numero:
     if numero % x == 0:
-        p += 1
+        d += 1
     x += 1
-if p > 1:
-    primo
-elif p == 1:
+if d == 1:
     primo = f"El número {numero} sí es primo"
 
 
